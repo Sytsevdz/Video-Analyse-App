c545ff602d95b7a2d61e896f4e2152385020322f
Sytsevdz <129111911+Sytsevdz@users.noreply.github.com>
2025-06-30 23:18:18 +0200

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 // Direct Supabase credentials for the ACC environment
 const supabaseUrl = 'https://nbsrkypcjkevncgzepqs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzIiwicmVmIjoibmJzcmt5cGNqa2V2bmNnemVwcXMiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTc1MDM0MjU0MCwiZXhwIjoyMDY1OTE4NTQwfQ.eIrHOXCzCfquju-LJibDjZZfVLt4hPQMNitY1kaO8jE';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5ic3JreXBjamtldm5jZ3plcHFzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzNDI1NDAsImV4cCI6MjA2NTkxODU0MH0.eIrHOXCzCfquju-LJibDjZZfVLt4hPQMNitY1kaO8jE';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
